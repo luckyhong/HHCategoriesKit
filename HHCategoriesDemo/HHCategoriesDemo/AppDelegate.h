8efc66c687f5dc4c6cdf9e23a89cdfaebadcc9ca
hanjihong <lucky_hanjihong@163.com>
2018-05-19 22:32:38 +0800

    created a repo

---
+++ HHCategoriesDemo/HHCategoriesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHCategoriesDemo
+//
+//  Created by Jihong Han on 2018/5/19.
+//  Copyright © 2018年 Jihong Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHCategoriesDemo/HHCategoriesDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHCategoriesDemo
+//
+//  Created by Jihong Han on 2018/5/19.
+//  Copyright © 2018年 Jihong Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
